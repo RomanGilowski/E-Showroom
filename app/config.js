b096d30ecae90b15eb07dbc02fc64d78522a51ef
RomanGilowski <sharker1wp@gmail.com>
2023-06-06 11:03:59 +0200

    ssl

--- app/config.js
+++ app/config.js
@@ -3,5 +3,6 @@ require('dotenv').config();
 module.exports = {
     port: process.env.PORT || 3002,
     database: process.env.DATABASE || 'mongodb://127.0.0.1:27017/katalog',
-    sessionKeySecret: process.env.SESSION_KEY_SECRET
+    sessionKeySecret: process.env.SESSION_KEY_SECRET,
+    ssl: prosess.env.SSL || false,
 }
--- app/index.js
+++ app/index.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const app = require('./app');
-const {port} = require('./config');
+const {port, ssl} = require('./config');
+const https = require('https');
+const fs = require('fs');
 
 app.listen(port, () => {
    console.log(`Serwer uruchomiony na porcie ${port}`)
 });
 
+// https 443
+if (ssl) {
+   https.createServer({
+      key: fs.readFileSync('/etc/letsencrypt/live/e-showroom.pl/privkey.pem'),
+      cert: fs.readFileSync('/etc/letsencrypt/live/e-showroom.pl/cert.pem'),
+      ca: fs.readFileSync('/etc/letsencrypt/live/e-showroom.pl/chain.pem'),
+   }, app).listen(443, () => {
+      console.log(`Serwer uruchomiony na porcie: 443`)
+   }) 
+}
