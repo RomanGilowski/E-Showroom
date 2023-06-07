a5a3ee7a13b07d1dc6e7e184efcc70ed1087eb4d
RomanGilowski <sharker1wp@gmail.com>
2023-06-07 09:19:44 +0200

    ssl-2

--- app/config.js
+++ app/config.js
@@ -4,5 +4,5 @@ module.exports = {
     port: process.env.PORT || 3002,
     database: process.env.DATABASE || 'mongodb://127.0.0.1:27017/katalog',
     sessionKeySecret: process.env.SESSION_KEY_SECRET,
-    ssl: prosess.env.SSL || false,
+    ssl: process.env.SSL || false,
 }
