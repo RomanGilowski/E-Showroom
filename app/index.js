3b9c67aa726b28a88008b7cbd8216502602919de
RomanGilowski <sharker1wp@gmail.com>
2023-06-09 11:03:58 +0200

    aktualizacja

--- app/index.js
+++ app/index.js
@@ -9,12 +9,4 @@ app.listen(port, () => {
 });
 
 // https 443
-if (ssl) {
-   https.createServer({
-      key: fs.readFileSync('/etc/letsencrypt/live/e-showroom.pl/privkey.pem'),
-      cert: fs.readFileSync('/etc/letsencrypt/live/e-showroom.pl/cert.pem'),
-      ca: fs.readFileSync('/etc/letsencrypt/live/e-showroom.pl/chain.pem'),
-   }, app).listen(443, () => {
-      console.log(`Serwer uruchomiony na porcie: 443`)
-   }) 
-}
+
